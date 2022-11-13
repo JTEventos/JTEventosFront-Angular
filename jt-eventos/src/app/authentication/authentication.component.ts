fa68476ac49b111bb5b716765682c686af5c8f3c
Thiago Proença <thiago_proenca@sicredi.com.br>
2022-11-13 18:39:01 -0300

    ajustes pontuais de layout

--- jt-eventos/src/app/authentication/authentication.component.ts
+++ jt-eventos/src/app/authentication/authentication.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
-  styleUrls: ['./authentication.component.css']
+  styleUrls: ['./authentication.component.css', '../../styles.css']
 })
 export class AuthenticationComponent implements OnInit {
+  title = 'Login'
 
   constructor() { }
 
